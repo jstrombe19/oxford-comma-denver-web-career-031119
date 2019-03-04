d2ea84398060596528fe5b7211f4839a12918894
Jared Stromberg <jaredstromberg@gmail.com>
2019-03-04 19:56:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,8 +8,8 @@ def oxford_comma(array)
   if array.length > 2
     i = 0
     new_array = []
-    while i < array.length - 1 do |ele|
-      new_array << ele
+    while i < array.length - 1 do
+      new_array << array[i]
       i += 1
     end
     oxford_string = new_array.join(", ")
